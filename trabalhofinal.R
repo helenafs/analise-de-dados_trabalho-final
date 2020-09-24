425789cf0934820cd18f14ce2a7dc475eae25ac5
Helena Santos <38816678+helenafs@users.noreply.github.com>
2020-09-24 14:03:05 -0300

    Add files via upload

--- trabalhofinal.R
+++ trabalhofinal.R
@@ -44,6 +44,7 @@ pessoas <- read.SAScii(pd, ps)
 #write_xlsx(pes, "pessoas.xlsx", use_zip64 = T)
 
 #Selecionar as colunas de interesse
+
 library(tidyverse)
 glimpse (pessoas)
 
@@ -77,58 +78,39 @@ banco_selecionado %>%
 #1    1   7738
 #2    2 197808
 
-#Recodificar as variáveis G001 (deficiência intelectual), G006 (deficiência física),
-#G014 (deficiência auditiva), G021 (deficiência visual).
-
-
-banco_deficiência <- banco_selecionado %>%
-  mutate(G001 = case_when  (G001 == "1" ~ "dintelectual",
-                           G001 == "2" ~ "não")) %>%
-  mutate(G006 = case_when(G006 == "1" ~ "dfísica",
-                          G006 == "2" ~ "não")) %>%
-  mutate(G014 = case_when(G014 == "1" ~"dauditiva",
-                          G014 == "2" ~ "não")) %>%
-  mutate(G021 = case_when(G021 == "1" ~ "dvisual",
-                          G021 == "2" ~ "não"))
-
-
 #Criar a variável de categorização PCD, categorias: intelectual, auditiva, visual, física 
 #sem deficiência (módulo g)
  
-banco_deficiência <- banco_selecionado %>%
-  mutate(deficiência = case_when(G001 == "1" ~ "dintelectual", 
+banco_deficiencia <- banco_selecionado %>%
+  mutate(deficiencia = case_when(G001 == "1" ~ "dintelectual", 
                                  G006 == "1" ~ "dfísica",
                                  G014 == "1" ~"dauditiva", 
                                  G021 == "1" ~ "dvisual",
                                  G001 == "2" & G006 == "2" & G014 == "2" & G021 == "2" ~ "nenhuma"))
-  
 
-table(banco_deficiência$deficiência)
-summary(banco_deficiência$deficiência)
+
+table(banco_deficiencia$deficiencia)
 
 #Criar outra variável com três grupos deficientes com limitação, deficientes sem limitação e 
 #não deficientes. As variáveis que indicam limitação: G004, G009, G017, G026. 
 
-banco_deficiência <- banco_deficiência %>%
-  mutate (limitação = case_when (G004 == "1" ~ "não limita", G004 == "2" ~ "um pouco", G004 == "3" ~ "moderadamente", G004 == "4" ~ "intensamente", G004 == "5" ~ "intensamente", 
-                                 G009 == "1" ~ "não limita", G009 == "2" ~ "um pouco", G009 == "3" ~ "moderadamente", G009 == "4" ~ "intensamente", G009 == "5" ~ "intensamente", 
-                                 G017 == "1" ~ "não limita", G017 == "2" ~ "um pouco", G017 == "3" ~ "moderadamente", G017 == "4" ~ "intensamente", G017 == "5" ~ "intensamente",
-                                 G026 == "1" ~ "não limita", G026 == "2" ~ "um pouco", G026 == "3" ~ "moderadamente", G026 == "4" ~ "intensamente", G026 == "5" ~ "intensamente")) 
+banco_deficiencia <- banco_deficiencia %>%
+  mutate (limita = case_when (G004 == "1" ~ "não limita", G004 == "2" ~ "um pouco", G004 == "3" ~ "moderadamente", G004 == "4" ~ "intensamente", G004 == "5" ~ "intensamente", 
+                              G009 == "1" ~ "não limita", G009 == "2" ~ "um pouco", G009 == "3" ~ "moderadamente", G009 == "4" ~ "intensamente", G009 == "5" ~ "intensamente", 
+                              G017 == "1" ~ "não limita", G017 == "2" ~ "um pouco", G017 == "3" ~ "moderadamente", G017 == "4" ~ "intensamente", G017 == "5" ~ "intensamente",
+                              G026 == "1" ~ "não limita", G026 == "2" ~ "um pouco", G026 == "3" ~ "moderadamente", G026 == "4" ~ "intensamente", G026 == "5" ~ "intensamente")) 
 
-banco_deficiência %>%
-  count (limitação)
+table(banco_deficiencia$limita)
 
 
 #Criar a variável de renda total (dependente): E01602 (salário principal), 
-#E01604 (produtos principal),E01802 (salário secundário), E01804 (produtos secundário).
+#E01604 (produtos principal),E01802 (salário secundário), E01804 (produtos secundários).
 
-is.numeric(banco_deficiência$VDF00102)
-
-banco_renda <- banco_deficiência %>% 
+banco_renda <- banco_deficiencia %>% 
   rowwise() %>% 
-  mutate(salário = sum(E01602,E01604,E01802,E01804, na.rm = TRUE))
+  mutate(salario = sum(E01602,E01604,E01802,E01804, na.rm = TRUE))
 
-summary(banco_renda$salário)
+summary (banco_renda$salario)
 
 #Criar a variável de controle de rendimentos domiciliares não provenientes do trabalho: 
 #F00102 (pensão do governo),F00702 (doação), F00802 (aluguel), VDF00102 (juros, seguros)
@@ -138,34 +120,193 @@ banco_renda <- banco_renda %>%
   rowwise() %>% 
   mutate(renda_domiciliar = sum(F00102,F00702,F00802,VDF00102, na.rm = TRUE))
 
-summary(banco_renda$renda_domiciliar)
+summary (banco_renda$renda_domiciliar)
 
-#Filtrar pela idade (C008)
+# Filtrar pela idade (C008) e renomear a variável 
 
 table(banco_renda$C008, banco_renda$VDE001)
 
 filtrado_idade <- banco_renda %>%
   mutate(C008 = as.numeric(C008))%>%
-  filter(C008 >= 14)
+  filter(C008 >= 14) %>%
+  rename(idade = C008)
 
-table(filtrado_idade$C008, filtrado_idade$VDE001)
+table(filtrado_idade$idade)
 
-is.numeric(banco_renda$C008)
+### Renomear e recodificar as variáveis: C006 (sexo), VDE001 (Condição em relação à força de trabalho na semana de referência para pessoas de 14 anos ou mais de idade), VDD004 (nível de instrução mais alto)
 
-#Recategorizar a variável de raça (C009), transformar em dummy - branco e não branco
-#Branco (1) = 0, não-branco (2, 3, 4, 5 e 9) = 1 (Becker, 2018). 
 
+filtrado_idade <- filtrado_idade %>%
+  mutate(sexo = case_when(C006 == "1" ~ "masculino",
+                          C006 == "2" ~ "feminino")) %>%
+  mutate (trabalho = case_when(VDE001 == "1" ~ "sim",
+                               VDE001 == "2" ~ "não")) %>%
+  mutate (escolaridade = case_when(VDD004 == "1" ~ "sem_instrução",
+                                   VDD004 == "2" ~ "fund_incompleto",
+                                   VDD004 == "3" ~ "fund_completo",
+                                   VDD004 == "4" ~ "médio_incompleto",
+                                   VDD004 == "5" ~ "médio_completo",
+                                   VDD004 == "6" ~ "sup_incompleto",
+                                   VDD004 == "7" ~ "sup_completo"))
+
+
+### Recategorizar a variável de cor (C009), transformar em dummy - branco e não branco. Branco (1), não-branco (2, 3, 4, 5 e 9) (Becker, 2018). 
+
+table(filtrado_idade$C009)
 
 filtrado_idade <- filtrado_idade %>% 
-  mutate(raça = case_when(C009 == "1" ~ "0",
-                          C009 == "2" ~ "1", 
-                          C009 == "3" ~ "1",
-                          C009 == "4" ~ "1",
-                          C009 == "5" ~ "1",
-                          C009 == "9" ~ "1"))
+  mutate(cor = case_when( C009 == "1" ~ "branco",
+                          C009 == "2" ~ "não_branco", 
+                          C009 == "3" ~ "não_branco",
+                          C009 == "4" ~ "não_branco",
+                          C009 == "5" ~ "não_branco",
+                          C009 == "9" ~ "não_branco" )) %>%
+  mutate(cor = as.factor(cor))
+
+levels(filtrado_idade$cor)
+table(filtrado_idade$cor)
+
+
+### Criar a categoria de região a partir da variável (V0001) de Estados. 
+
+filtrado_idade <- filtrado_idade %>% 
+  mutate(regiao = case_when(V0001 >= 11 & V0001 < 18 ~ "Norte",
+                            V0001 >= 21 & V0001 < 30 ~ "Nordeste",
+                            V0001 >= 31 & V0001 < 36 ~ "Sudeste",
+                            V0001 >= 41 & V0001 < 44 ~ "Sul",
+                            V0001 >= 50 & V0001 < 54 ~ "Centro-oeste")) %>%
+  mutate(regiao = as.factor(regiao))
+
+levels(filtrado_idade$regiao)
+
+### Variável de ocupação (VDE)
+
+### Mudar as categorias de referência da variável deficiência e escolaridade 
+
+filtrado_idade$deficiencia <- fct_relevel(filtrado_idade$deficiencia, 
+                                          "nenhuma")
+
+filtrado_idade$escolaridade <- fct_relevel(filtrado_idade$escolaridade, 
+                                           "sem_instrução")
+
+
+### Regressão 1, com a variável de deficiencia como dependente.
+### Fazer interação entre cor e sexo 
+
+regressao1 <- lm (salario ~ deficiencia + idade + sexo + trabalho + escolaridade + 
+                    renda_domiciliar + cor + regiao, data = filtrado_idade)
+summary(regressao1)
+confint(regressao1)
+
+### Verificar os pressupostos da regressão 1
+
+### Linearidade 
+
+plot(regressao1, 1)
+
+### Homocedasticidade
+
+plot(regressao1, 3)
+
+library(lmtest)
+
+bptest(regressao1)
+
+library(car)
+
+ncvTest(regressao1) 
+
+### Autocorrelação entre casos/resíduos
+
+acf(regressao1$residuals)
+durbinWatsonTest(regressao1)
+
+### Normalidade dos Resíduos
+
+plot(regressao1, 2)
+
+###Multicolinearidade
+
+library (car)
+vif (regressao1)
+
+### Outlier/Observação influente
+
+plot (regressao1, 4)
+
+plot (regressao1, 5)
+
+sem_outlier <- filtrado_idade %>%
+  slice (-c(111104, 111090, 137068))
+
+regressao2 <- lm (salario ~ deficiencia + idade + sexo + trabalho + escolaridade + 
+                    renda_domiciliar + cor + regiao, data = sem_outlier)
+summary(regressao2)
+confint(regressao2)
+
+### Regressão 3, com a variável de limitação como dependente
+
+regressao3 <- lm (salario ~ limita + idade + sexo + trabalho + 
+                  escolaridade + renda_domiciliar + cor + regiao, 
+                  data = filtrado_idade)
+
+summary(regressao3)
+confint(regressao3)
+
+### Linearidade 
+
+plot(regressao3, 1)
+
+### Homocedasticidade
+
+plot(regressao3, 3)
+
+library(lmtest)
+
+bptest(regressao2)
+
+library(car)
+
+ncvTest(regressao2) 
+
+
+### Autocorrelação entre casos/resíduos
+
+
+
+acf(regressao3$residuals)
+durbinWatsonTest(regressao3)
+
+
+
+### Normalidade dos Resíduos
+
+
+
+plot(regressao3, 2)
+
+
+
+###Multicolinearidade
+
+library (car)
+vif (regressao3)
+
+
+### Outlier/Observação influente
+
+plot (regressao3, 4)
+
+plot (regressao3, 5)
+
+sem_outlier2 <- filtrado_idade %>%
+  slice (-c(101225,123432,148567))
+
+regressao4 <- lm (salario ~ limita + idade + sexo + trabalho + escolaridade + renda_domiciliar + cor + regiao, data = sem_outlier2)
+summary(regressao4)
+confint(regressao4)
 
 #Transformar em .RMD
-
 
 library(magrittr)
 
@@ -173,6 +314,5 @@ readLines('trabalhofinal.R') %>%
   stringr::str_replace("# chunkstart", "```{r}") %>% 
   stringr::str_replace("# chunkend", "```") %>% 
   writeLines("trabalhofinal.Rmd")
-
 
 
